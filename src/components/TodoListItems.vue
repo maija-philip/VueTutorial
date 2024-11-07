9682dc4c5d89b6da2419d58d55d69fc764aa5af1
Sophia Castiglione <src8776@rit.edu>
2024-11-07 18:28:26 -0500

    made todo list draggable

--- src/components/TodoListItems.vue
+++ src/components/TodoListItems.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
 import TodoItem from './TodoItem.vue'
 import { useTaskStore } from '@/stores/tasks'
+import draggable from 'vuedraggable'
 
 const taskStore = useTaskStore()
 </script>
 
 <template>
   <p v-if="taskStore.tasks.length < 1">Nothing todo yet</p>
-  <div class="todo-list-items">
-    <TodoItem
-      v-for="(task, index) in taskStore.tasks"
-      :key="index"
-      :name="task"
-      :index="index"
-    />
-  </div>
+  <draggable v-model="taskStore.tasks" tag="div" class="todo-list-items">
+    <template #item="{element, index}">
+      <TodoItem
+        :key="index"
+        :name="element"
+        :index="index"
+      />
+    </template>
+  </draggable>
 </template>
 
 <style scoped></style>
